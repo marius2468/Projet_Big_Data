edb11c8b871d0b412cf3c81a805fb85834d296ea
marius2468 <mariusfremond@isen-ouest.yncrea.fr>
2023-06-13 10:11:00 +0200

    cc

--- Prepa_Data.R
+++ Prepa_Data.R
@@ -57,6 +57,4 @@ acf(accidents_par_mois_counts)
 accidents_par_semaine_counts <- table(format(data$date, "%Y-%U"))
 plot(accidents_par_semaine_counts, type = "l")
 acf(accidents_par_semaine_counts)
- 
-
 
